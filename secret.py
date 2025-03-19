676d1ae919ee5f9ba9896d3e59f3b21eaf968dd7
Mike Anselmi <git@manselmi.com>
2025-03-19 10:30:10 -0400

    Allow for reading secrets from files

--- secret.py
+++ secret.py
@@ -1,7 +1,7 @@
 # vim: set ft=python :
 
 """
-Read a secret from a program's standard output.
+Read secrets from various sources.
 
 Pydantic Secret Types documentation:
 https://docs.pydantic.dev/latest/usage/types/#secret-types
@@ -11,7 +11,9 @@ import os
 import shlex
 import subprocess
 from collections.abc import Sequence
+from pathlib import Path
 from subprocess import PIPE
+from typing import TypedDict, Unpack
 
 import structlog
 from pydantic.types import SecretStr
@@ -25,6 +27,12 @@ Secret = SecretStr
 class EnvLookupError(Exception):
     def __init__(self, name: str) -> None:
         super().__init__(f"environment variable name not found: {name}")
+
+
+class _ReadTextKwargs(TypedDict, total=False):
+    encoding: str | None
+    errors: str | None
+    newline: str | None
 
 
 def secret_cmd_argv(argv: Sequence[str]) -> Secret:
@@ -85,3 +93,33 @@ def secret_env_var(name: str) -> Secret:
     except KeyError as exc:
         log.exception("secret_env_var_is_unset")
         raise EnvLookupError(name) from exc
+
+
+def secret_file(path: Path, **read_text_kwargs: Unpack[_ReadTextKwargs]) -> Secret:
+    """
+    Read a secret from the given path.
+
+    Example:
+        >>> from contextlib import redirect_stdout
+        >>> from os import devnull
+        >>> from tempfile import NamedTemporaryFile
+        >>> encoding = "utf-8"
+        >>> with NamedTemporaryFile(mode="w+t", encoding=encoding) as secret_fobj:
+        ...     with open(devnull, mode="wt") as devnull_fobj, redirect_stdout(devnull_fobj):
+        ...         secret_fobj.write("secret_value")
+        ...     secret_fobj.flush()
+        ...     with structlog.testing.capture_logs():
+        ...         secret = secret_file(Path(secret_fobj.name), encoding=encoding)
+        >>> print(secret.get_secret_value())
+        secret_value
+    """
+    log = logger.bind(secret_file=path)
+    log.debug("read_secret_file")
+    try:
+        return Secret(path.read_text(**read_text_kwargs))
+    except FileNotFoundError:
+        log.exception("secret_file_not_found")
+        raise
+    except PermissionError:
+        log.exception("secret_file_bad_perms")
+        raise
